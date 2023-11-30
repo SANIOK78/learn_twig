3b807ea8c80f75409406ae92ddec236ba7c1ab4e
Alexandru Ghilev <aghilev@yahoo.fr>
2023-11-30 23:48:42 +0100

    Variables globales

--- config/twig.php
+++ config/twig.php
@@ -5,3 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+
+//On ajoute la globale $email à l'initialisation de l'objet Twig
+$email = 'contact@yahoo.fr';
+$twig->addGlobal('contactEmail', $email);
