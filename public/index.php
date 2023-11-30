5308a86d6d16303d8fa350726a71baec5016f4ec
Alexandru Ghilev <aghilev@yahoo.fr>
2023-11-30 21:34:38 +0100

    Heritage avec Twig

--- public/index.php
+++ public/index.php
@@ -3,7 +3,4 @@
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
 
-
-$name = 'Wilder';
-
-echo $twig->render('home.html.twig', ['toto' => $name]);
+echo $twig->render('home.html.twig');
